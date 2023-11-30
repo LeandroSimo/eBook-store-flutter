79ae859eb99000eb17eedde8a2a0b634c66f8eed
LeandroSimo <leo.bt.12@hotmail.com>
2023-11-29 23:25:09 -0300

    feat: consuming information from the api

--- lib/src/presentation/pages/book_lista_page.dart
+++ lib/src/presentation/pages/book_lista_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teste_escribo_app/src/stores/book_store.dart';
 
 class BookListPage extends StatefulWidget {
   @override
@@ -6,24 +7,89 @@ class BookListPage extends StatefulWidget {
 }
 
 class _BookListPageState extends State<BookListPage> {
+  final bookStore = BookStore();
+
+  @override
+  void initState() {
+    super.initState();
+    bookStore.addListener(_listener);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      bookStore.fetchBooks();
+    });
+  }
+
+  void _listener() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    bookStore.removeListener(_listener);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(8),
-        child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-              childAspectRatio: 3 / 3.5,
-            ),
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Container(
-                color: Colors.red,
-              );
-            }),
+      backgroundColor: Colors.deepPurple.shade100,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                childAspectRatio: 3 / 3.5,
+              ),
+              itemCount: bookStore.value.books.length,
+              itemBuilder: (context, index) {
+                final book = bookStore.value.books[index];
+                return Card(
+                    elevation: 4,
+                    color: Colors.deepPurple,
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: Image.network(
+                            book.cover_url,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.black54,
+                            child: Column(
+                              children: [
+                                Text(
+                                  book.title,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  book.author,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ));
+              }),
+        ),
       ),
     );
   }
