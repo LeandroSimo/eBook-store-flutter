fb4472b376108a842918121eb5a2d1864ff8d09e
LeandroSimo <leo.bt.12@hotmail.com>
2023-11-30 19:36:23 -0300

    feat: adjusting card of the book cover

--- lib/src/presentation/pages/book_list_favorites_page.dart
+++ lib/src/presentation/pages/book_list_favorites_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:teste_escribo_app/src/presentation/widgets/card_cover_book.dart';
 import 'package:teste_escribo_app/src/stores/book_store_provider.dart';
 
 class BookListFavoritesPage extends StatelessWidget {
@@ -30,68 +31,12 @@ class BookListFavoritesPage extends StatelessWidget {
                     crossAxisCount: 2,
                     crossAxisSpacing: 8,
                     mainAxisSpacing: 8,
-                    childAspectRatio: 3 / 3.5,
+                    childAspectRatio: 3 / 5,
                   ),
                   itemCount: books.length,
                   itemBuilder: (_, index) {
                     final book = books[index];
-                    return Card(
-                      elevation: 4,
-                      child: Stack(
-                        children: [
-                          Positioned.fill(
-                            child: Image.network(
-                              book.cover_url,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            left: 0,
-                            right: 0,
-                            child: Container(
-                              padding: const EdgeInsets.all(8),
-                              color: Colors.black54,
-                              child: Column(
-                                children: [
-                                  Text(
-                                    book.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    book.author,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            top: 0,
-                            right: 0,
-                            child: IconButton(
-                              icon: Icon(
-                                _provider.isFavorite(book)
-                                    ? Icons.favorite
-                                    : Icons.favorite_border,
-                                color: _provider.isFavorite(book)
-                                    ? Colors.red
-                                    : Colors.white,
-                              ),
-                              onPressed: () => _provider.toggleFavorite(book),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
+                    return CardCoverBook(book: book, provider: _provider);
                   },
                 ),
         ),
---
+++ lib/src/presentation/widgets/card_cover_book.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:teste_escribo_app/src/models/book_model.dart';
+import 'package:teste_escribo_app/src/stores/book_store_provider.dart';
+
+class CardCoverBook extends StatelessWidget {
+  const CardCoverBook({
+    super.key,
+    required this.book,
+    required BookStoreProvider provider,
+  }) : _provider = provider;
+
+  final BookModel book;
+  final BookStoreProvider _provider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      color: const Color(0xFF1D1244),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  image: NetworkImage(book.cover_url),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 4,
+                        child: Text(
+                          book.title,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                          ),
+                          maxLines: 2,
+                        ),
+                      ),
+                      Expanded(
+                        child: IconButton(
+                          onPressed: () => _provider.toggleFavorite(book),
+                          icon: Icon(
+                            _provider.isFavorite(book)
+                                ? Icons.favorite
+                                : Icons.favorite_border,
+                            color: _provider.isFavorite(book)
+                                ? Colors.red
+                                : Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    book.author,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                    maxLines: 1,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
