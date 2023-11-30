e2eac905fd2f6cd992222927d6ce0f13ce4497b3
LeandroSimo <leo.bt.12@hotmail.com>
2023-11-29 22:57:14 -0300

    feat: adding grid view

--- lib/src/presentation/pages/book_lista_page.dart
+++ lib/src/presentation/pages/book_lista_page.dart
@@ -9,8 +9,21 @@ class _BookListPageState extends State<BookListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Book List'),
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 3 / 3.5,
+            ),
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                color: Colors.red,
+              );
+            }),
       ),
     );
   }
